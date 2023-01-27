76aa5a17da639aae02d4acf18d58d388800653d2
gmmmmd <zoopark86@yandex.ru>
2023-01-27 20:21:52 +0400

    feat: add complite task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return [];
+  }
+  matrix = matrix.map((item, index) => {
+    if (index % 2 !== 0) {
+      return item.reverse();
+    }
+    return item;
+    }).flat(Infinity);
+  return matrix;
+};
